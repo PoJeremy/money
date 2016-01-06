88a8a959789228cfdef0eda1da3f246ca3989923
F1ReKing <284061805@qq.com>
2016-01-06 20:32:39 +0800

    add greendao

---
+++ daoexamplegenerator/src/main/java/com/example/ExampleDaoGenerator.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import de.greenrobot.daogenerator.Schema;
+
+public class ExampleDaoGenerator {
+
+    public static void main(String[] args) throws Exception{
+
+        Schema schema = new Schema(1,"com.f1reking.greendao");
+
+
+
+
+
+    }
+}
