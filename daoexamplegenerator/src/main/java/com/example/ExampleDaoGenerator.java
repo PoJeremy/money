3e020774bb7a86e77c4b993264e1dec7b13971fc
F1ReKing <284061805@qq.com>
2016-01-06 22:08:37 +0800

    add greendao

--- daoexamplegenerator/src/main/java/com/example/ExampleDaoGenerator.java
+++ daoexamplegenerator/src/main/java/com/example/ExampleDaoGenerator.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
 import de.greenrobot.daogenerator.Schema;
 
 public class ExampleDaoGenerator {
@@ -8,9 +10,18 @@ public class ExampleDaoGenerator {
 
         Schema schema = new Schema(1,"com.f1reking.greendao");
 
+        addNote(schema);
+
+        new DaoGenerator().generateAll(schema,"E:/workspace/Android Studio/Money/mymoney/src/main/java-gen");
 
 
+    }
 
-
+    private static void addNote(Schema schema) {
+        Entity note = schema.addEntity("Note");
+        note.addIdProperty();
+        note.addStringProperty("text").notNull();
+        note.addStringProperty("comment");
+        note.addDateProperty("date");
     }
 }
