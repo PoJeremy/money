2b625f53acf3f7d504186ad7a468b625d715a4a6
F1ReKing <284061805@qq.com>
2016-01-09 22:09:42 +0800

    update

--- mymoney/src/main/java/com/f1reking/mymoney/ui/activity/MainActivity.java
+++ mymoney/src/main/java/com/f1reking/mymoney/ui/activity/MainActivity.java
@@ -1,9 +1,11 @@
-package com.f1reking.mymoney;
+package com.f1reking.mymoney.ui.activity;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import com.f1reking.mymoney.R;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
