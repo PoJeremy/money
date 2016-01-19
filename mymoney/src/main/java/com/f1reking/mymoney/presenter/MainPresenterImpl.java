4205561622566f7a231b2a65da71a72550f17917
F1ReKing <284061805@qq.com>
2016-01-19 23:16:32 +0800

    update

---
+++ mymoney/src/main/java/com/f1reking/mymoney/presenter/MainPresenterImpl.java
@@ -0,0 +1,37 @@
+package com.f1reking.mymoney.presenter;
+
+import com.f1reking.mymoney.R;
+import com.f1reking.mymoney.view.MainView;
+
+/**
+ * Created by F1ReKing on 2016/1/19.
+ */
+public class MainPresenterImpl implements MainPresenter {
+
+    private MainView mMainView;
+
+    public MainPresenterImpl(MainView mainView){
+        this.mMainView = mainView;
+    }
+
+    @Override
+    public void switchNavigation(int id) {
+        switch (id){
+            case R.id.navigation_item_home:
+                mMainView.switchToHome();
+                break;
+            case R.id.navigation_item_category:
+                mMainView.switchToCategory();
+                break;
+            case R.id.navigation_item_report:
+                mMainView.switchToReport();
+                break;
+            case R.id.navigation_item_setting:
+                mMainView.switchToSetting();
+                break;
+            default:
+                mMainView.switchToHome();
+                break;
+        }
+    }
+}
