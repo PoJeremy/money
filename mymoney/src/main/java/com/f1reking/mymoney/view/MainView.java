01dd451912b5cf7a62a969cf8627e78e674307bd
F1ReKing <284061805@qq.com>
2016-01-19 23:12:40 +0800

    update

---
+++ mymoney/src/main/java/com/f1reking/mymoney/view/MainView.java
@@ -0,0 +1,11 @@
+package com.f1reking.mymoney.view;
+
+/**
+ * Created by F1ReKing on 2016/1/19.
+ */
+public interface MainView {
+    void switchToHome();
+    void switchToCategory();
+    void switchToReport();
+    void switchToSetting();
+}
