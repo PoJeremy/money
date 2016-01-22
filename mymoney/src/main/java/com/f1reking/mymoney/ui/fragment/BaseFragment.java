bcbbfa89a762ed7c9a467c0068b20d1446890fcf
F1ReKing <284061805@qq.com>
2016-01-22 23:53:02 +0800

    update

--- mymoney/src/main/java/com/f1reking/mymoney/ui/fragment/BaseFragment.java
+++ mymoney/src/main/java/com/f1reking/mymoney/ui/fragment/BaseFragment.java
@@ -1,11 +1,29 @@
 package com.f1reking.mymoney.ui.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by F1ReKing on 2016/1/11.
  */
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
 
+    protected View rootView;
+    protected abstract void initViews();
+    protected abstract void initDatas();
+    protected abstract int getContentViewId();
 
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        rootView = inflater.inflate(getContentViewId(),container,false);
+        ButterKnife.bind(this,rootView);
+        initViews();
+        initDatas();
+        return rootView;
+    }
 }
