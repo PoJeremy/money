6b4259e413d2d07b91462895bf418e1695bbdf63
F1ReKing <284061805@qq.com>
2016-01-22 23:30:00 +0800

    update

--- mymoney/src/main/java/com/f1reking/mymoney/ui/fragment/ReportFragment.java
+++ mymoney/src/main/java/com/f1reking/mymoney/ui/fragment/ReportFragment.java
@@ -1,7 +1,20 @@
 package com.f1reking.mymoney.ui.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.f1reking.mymoney.R;
+
 /**
  * Created by F1ReKing on 2016/1/19.
  */
 public class ReportFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_report,container,false);
+        return view;
+    }
 }
