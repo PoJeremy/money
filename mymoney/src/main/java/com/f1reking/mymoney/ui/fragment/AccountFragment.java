e754bae8e3eacf4efccc8a9f83623ea71fbede71
F1ReKing <284061805@qq.com>
2016-01-24 23:55:21 +0800

    update

--- mymoney/src/main/java/com/f1reking/mymoney/ui/fragment/AccountFragment.java
+++ mymoney/src/main/java/com/f1reking/mymoney/ui/fragment/AccountFragment.java
@@ -1,15 +1,13 @@
 package com.f1reking.mymoney.ui.fragment;
 
-import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.design.widget.Snackbar;
 
 import com.f1reking.mymoney.R;
 
 import butterknife.Bind;
-import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by F1ReKing on 2016/1/19.
@@ -18,6 +16,8 @@ public class AccountFragment extends BaseFragment {
 
     @Bind(R.id.fab_record)
     FloatingActionButton mFabRecord;
+    @Bind(R.id.coordinatorLayout)
+    CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected int getContentViewId() {
@@ -26,19 +26,16 @@ public class AccountFragment extends BaseFragment {
 
     @Override
     protected void initViews() {
+
     }
 
     @Override
     protected void initDatas() {
-
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO: inflate a fragment view
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
-        ButterKnife.bind(this, rootView);
-        return rootView;
+    @OnClick(R.id.fab_record)
+    void onclick() {
+        Snackbar.make(mCoordinatorLayout, "点击", Snackbar.LENGTH_SHORT).show();
     }
 
 
