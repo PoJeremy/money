d6bff99b5864009b0aa1ee2f62d242df5d3610cb
F1ReKing <284061805@qq.com>
2016-02-01 23:22:26 +0800

    update

---
+++ mymoney/src/main/java/com/f1reking/mymoney/ui/activity/RecordActivity.java
@@ -0,0 +1,17 @@
+package com.f1reking.mymoney.ui.activity;
+
+/**
+ * Created by F1ReKing on 2016/2/1.
+ */
+public class RecordActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutRes() {
+        return 0;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
